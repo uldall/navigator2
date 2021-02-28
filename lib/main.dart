60c8a2d78fb5a0aa7ff154ecb60d177d388d4c08
Christian Uldall Pedersen <cup@gamblify.com>
2021-02-28 17:49:12 +0100

    Added left menu.

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,35 @@ class MyApp extends StatelessWidget {
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
+        // changing the parimarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainWidget(),
     );
+  }
+}
+
+class MainWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      children: [
+        SizedBox(width: 200, child: MyLeftBar()),
+        Expanded(child: MyHomePage(title: 'Flutter Demo Home Page')),
+      ],
+    ));
+  }
+}
+
+class MyLeftBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.greenAccent);
   }
 }
 
